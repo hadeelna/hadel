64b296207ff55c758671384f9aa10eb672447f99
hadeelna <82890331+hadeelna@users.noreply.github.com>
2021-09-18 22:05:03 +0300

    Add files via upload

--- gr_list.js
+++ gr_list.js
@@ -1,13 +1,13 @@
+
 const gr2 = require("./gr2.js");
-const yargs = require("yargs")
+const yargs = require('yargs')
 
-
-    .option("chilled", {
+    .option("chilled",{
       alias: "c",
       describe: " add to chilled catogery ",
       type: "string"
     })
-    .option("dry", {
+    .option( "dry", {
         alias: "d",
         describe: "add to dry catogery",
         type: "string"
@@ -20,6 +20,7 @@ const yargs = require("yargs")
       .option("fruits", {
         alias: "f",
         describe: "add to fruits catogery",
+
         type: "string"
       })
       .option("breads", {
@@ -27,15 +28,12 @@ const yargs = require("yargs")
         describe: "add to breads catogery",
         type: "string"
       })
+ 
     .array("chilled")
     .array("dry")
     .array("vegetables")
     .array("fruits")
     .array("breads")
-    let argv = yargs.argv;
     
- 
-function handler (argv)
-{
-   list.addtolist(argv.catogery,argv.proudect);
-}
+    let argv = yargs.argv;
+    console.log(argv)
